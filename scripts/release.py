ad8f66e1192594c9460f933b8f9fb1da2d159515
Andrew Andrianov <andrew@ncrmnt.org>
2022-11-02 16:25:40 +0300

    scripts: Update release script to drop github push
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- scripts/release.py
+++ scripts/release.py
@@ -30,9 +30,9 @@ os.system("git add rumboot/__init__.py")
 os.system(f'git commit -s -m \"v{newversion} release\"')
 os.system(f'git tag v{newversion}')
 os.system(f"git push origin master")
-os.system(f"git push github master")
+#os.system(f"git push github master")
 os.system(f"git push origin v{newversion}")
-os.system(f"git push github v{newversion}")
+#os.system(f"git push github v{newversion}")
 
 if os.access('dist',os.R_OK):
     shutil.rmtree('dist')
